7ad26d94c2f045d17772bda5a5fba1816ef13b4a
liuxin.rkl <liuxin.rkl@taobao.com>
2016-01-25 12:13:15 +0800

    trace dump button

--- example/index.jsx
+++ example/index.jsx
@@ -2,11 +2,4 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var App = require('./school');
 
-var Filofax = require('../src/filofax');
-var trace = new Filofax();
-window.onerror = function (msg, url, line, column, err) {
-  trace.shot(err);
-  trace.dump();
-}
-
-ReactDOM.render(<App trace = {trace} />, document.getElementById('container'));
+ReactDOM.render(<App />, document.getElementById('container'));
--- example/school.jsx
+++ example/school.jsx
@@ -1,17 +1,29 @@
 var React = require('react');
 var Fruit = require('./fruit');
 
+var Filofax = require('../src/filofax');
+var trace = new Filofax();
+window.onerror = function (msg, url, line, column, err) {
+  trace.shot(err);
+  trace.dump();
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  handleClick() {
+    trace.dump();
+  }
+
   render() {
-    this.props.trace.shot({type: 'root'});
+    trace.shot({type: 'root'});
     return (
       <div>
-        <Fruit trace = {this.props.trace} type="苹果" count='30' />
-        <Fruit trace = {this.props.trace} type="梨" count='10' />
+        <button onClick={this.handleClick.bind(this)}>trace dump</button>
+        <Fruit trace = {trace} type="苹果" count='30' />
+        <Fruit trace = {trace} type="梨" count='10' />
       </div>
     );
   }
